840a02f73ed54b899c76118c6104f4e30415ca4a
Ibrahim <abdulkadiribraheemolushola@gmail.com>
2022-12-23 17:30:31 +0100

    changes made successfully

---
+++ gaming.js
@@ -0,0 +1,8 @@
+const list = document.getElementById('list');
+const menu = document.getElementById('menu');
+const close=document.getElementById('close');
+menu.addEventListener('click',show);
+function show(){
+  list.style.fontSize ='12px';
+  list.style.backgroundcolor ='white';
+}
